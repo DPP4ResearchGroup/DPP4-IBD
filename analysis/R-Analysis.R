61bbed203a92f4e0ca12def5a6113c087ac9fb4c
Robert Qiao <robert.qiao@adelaide.edu.au>
2017-09-25 21:07:14 +0930

    analysis update

--- analysis/R-Analysis.R
+++ analysis/R-Analysis.R
@@ -2,6 +2,8 @@ install.packages("readxl")
 library("readxl")
 install.packages("car")
 library(car)
+install.packages("RColorBrewer")
+library(RColorBrewer)
 
 # Dataset construction 
 dataset = read_excel("IBD_data/IBD.xlsx", sheet= "Sheet2")
@@ -9,5 +11,11 @@ NonIBD<-dataset[1:8,]
 CD<-dataset[9:16,]
 UC<-dataset[17:23,]
 
-# Analysis 
-scatterplotMatrix(NonIBD)
+# Correlation Plot Analysis 
+NonIBDplot<-scatterplotMatrix(NonIBD)
+CDplot<-scatterplotMatrix(CD)
+UCplot<-scatterplotMatrix(UC)
+
+# Data Profile Plot
+names <- c("DPP4", "FAP", "DPP8", "DPP9", "DPP2")
+makeProfilePlot(NonIBD,names)
